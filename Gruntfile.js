35c8f6865f6e05c3df2c6b0b0ec58f47ac5fc099
Mike Calvanese <mike@tiny-asus.(none)>
2015-03-30 23:53:02 -0400

    Boilerplate angular component code

---
+++ Gruntfile.js
@@ -0,0 +1,160 @@
+/* global module */
+
+module.exports = function(grunt) {
+
+    var shell = require('shelljs');
+
+    var gruntConfig = {
+        pkg: grunt.file.readJSON('package.json'),
+
+        karma: {
+            unit: {
+                configFile: './karma.conf.js',
+                autoWatch: false,
+                singleRun: true,
+                browsers: ['PhantomJS']
+            }
+        },
+
+        jshint: {
+            options: {
+                browser: true,
+                maxlen: 120,
+                unused: true,
+                undef: true,
+                globals: {
+                    angular: false
+                }
+            },
+            src: {
+                files: {
+                    src: [
+                        'src/**/*.js',
+                        '!src/**/*_test.js'
+                    ]
+                }
+            }
+        },
+
+        clean: {
+            dist: ['dist/*.js', 'dist/*.json']
+        },
+
+        copy: {
+            unmin: {
+                src: 'src/component.js',
+                dest: 'dist/component.js'
+            },
+            bower: {
+                src: 'bower.json',
+                dest: 'dist/bower.json'
+            }
+        },
+
+        less: {
+            demo: {
+                files: {
+                    'demo/demo.css': 'demo/demo.less'
+                }
+            }
+        },
+
+        uglify: {
+            component: {
+                files: {
+                    'dist/component.min.js': ['src/component.js']
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true
+            },
+            rebuild: {
+                files: [
+                    'src/**/*',
+                    'demo/**/*'
+                ],
+                tasks: 'build'
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    livereload: true
+                }
+            }
+        }
+    };
+
+    grunt.initConfig(gruntConfig);
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('check', ['jshint']);
+
+    grunt.registerTask('test', ['karma']);
+
+    grunt.registerTask('package', [
+        'clean',
+        'less',
+        'copy',
+        'uglify'
+    ]);
+
+    grunt.registerTask('build', [
+        'check',
+        'test',
+        'package'
+    ]);
+
+    grunt.registerTask('release-prepare', 'Set up submodule to receive a new release',
+    function() {
+        // Make sure we have the submodule in dist
+        run("git submodule init");
+        run("git submodule update");
+        run("cd dist; git checkout master");
+    });
+
+
+    grunt.registerTask('release-commit', 'push new build to bower component repo',
+    function() {
+        // Stamp version
+        var comp = grunt.file.readJSON('bower.json');
+        var newVer = comp.version;
+        grunt.log.writeln("Version: " + newVer);
+        // Commit submodule
+        run('cd dist; git commit -a -m"Build version '+ newVer +'"', "Commited to bower repo");
+        // Tag submodule
+        run('cd dist; git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged bower repo");
+        // Commit and tag this.
+        run('git commit -a -m"Build version '+ newVer +'"', "Commited to source repo");
+        run('git tag ' + newVer + ' -m"Release version '+ newVer +'"', "Tagged source repo");
+        run("git submodule update");
+        // push?
+        grunt.log.ok("DON'T FORGET TO PUSH BOTH!");
+    });
+
+    grunt.registerTask('release', [
+        'release-prepare',
+        'package',
+        'release-commit'
+    ]);
+
+    grunt.registerTask('default', [
+        'build',
+        'connect',
+        'watch'
+    ]);
+    
+    function run(cmd, msg){
+        shell.exec(cmd, {silent:true});
+        if( msg ){
+            grunt.log.ok(msg);
+        }
+    }
+
+};
---
+++ demo/demo.js
@@ -0,0 +1,3 @@
+window.demoApp = angular.module('demoApp', [
+    'my-module'
+]);
